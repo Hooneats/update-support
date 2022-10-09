336672427e208bcc5ff35d509c59e9f6622661df
hooneats <hooneats@naver.com>
2022-10-09 18:30:21 +0900

    init commit V01

--- src/main/java/com/hooneats/support/mapper/UpdateColumn.java
+++ src/main/java/com/hooneats/support/mapper/UpdateColumn.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UpdateColumn {
 
-  String updateFieldName() default "";
+  String name() default "";
 
 }
